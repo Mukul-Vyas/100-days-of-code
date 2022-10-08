30d98c61605817af7e4afe0300ee901b7d98c4fd
Mukul-Vyas <85154842+Mukul-Vyas@users.noreply.github.com>
2022-10-08 23:47:30 +0530

    Find Occorance

---
+++ findoccorance.java
@@ -0,0 +1,31 @@
+public class findoccorance {
+
+    public static int first=-1;
+    public static int last=-1;
+
+    public static void findoccorance(String str, int idx, char element){
+
+        if(idx==str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+
+
+        int curr_char=str.charAt(idx);
+        if(curr_char==element){
+            if(first==-1){
+                first=idx;
+            }else{
+                last=idx;
+            }
+
+        }
+        findoccorance(str,idx+1,element);
+    }
+    public static void main(String[] args) {
+        String name ="abbjajaaha";
+        findoccorance(name,0,'a');
+
+    }
+}
